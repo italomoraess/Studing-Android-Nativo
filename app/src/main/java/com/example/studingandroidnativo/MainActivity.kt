419b037d9b4793c4c26112c217128ad39021e16a
Italo Moraes <italomoraes.sth@gmail.com>
2023-10-03 22:02:59 -0300

    Ciclo de vida de uma activity

--- app/src/main/java/com/example/studingandroidnativo/MainActivity.kt
+++ app/src/main/java/com/example/studingandroidnativo/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.studingandroidnativo
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +11,8 @@ class MainActivity : AppCompatActivity() {
     lateinit var btnAbrir: Button;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.i("ciclo_vida", "onCreate");
+
         setContentView(R.layout.activity_main)
 
         btnAbrir = findViewById(R.id.btn_abrir);
@@ -17,5 +20,17 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, DetaisActivity::class.java);
             startActivity(intent)
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("ciclo_vida", "onStart");
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("ciclo_vida", "onResume");
+
     }
 }
